f16b8f3dac9039ab5ac5b221ddd8ff2c9b9feb08
maxzadi <maxzadi20@gmail.com>
2026-05-04 17:38:34 -0600

    solucionador completo

--- GaussJordan.py
+++ GaussJordan.py
@@ -1,17 +1,65 @@
 import numpy as np
+
 class GaussJordan:
-    def __init__(self,ecuaciones):
-        self.x = ecuaciones     #MATRIZ
+    def __init__(self):
+        self.x = None
+        
+    def nuevo(self, sistema):
+        self.x = np.array(sistema, dtype=float) 
     
     def resolver(self):
-        x = self.x              #MATRIZ
-        m = len(x)              #CANTIDAD DE ECUACIONES
-        n = len(x[0])-1           #CANTIDAD DE VARIABLES
-        if self.verificar1(m,n): raise Exception("","")
-    
-    
-#================== CASOS ESPECIALES ==================
-    
-    #CASO 1: Más variables que ecuaciones
-    def verificar1(self,m,n):
-        return m<n
+        m = len(self.x)         #FILAS
+        n = len(self.x[0]) - 1  #VARIABLES
+        
+        i = 0  # fila
+        j = 0  # columna
+        
+        pivotes = 0
+        
+        # ELIMINACIÓN GAUSSIANA
+        while i < m and j < n:
+            
+            pivot = None
+            for k in range(i, m):
+                if not np.isclose(self.x[k, j], 0):
+                    pivot = k
+                    break
+            
+            if pivot is None:
+                j += 1
+                continue
+            
+            if pivot != i:
+                self.x[[i, pivot]] = self.x[[pivot, i]]
+            
+            self.x[i] = self.x[i] / self.x[i, j]
+            for k in range(i+1, m):
+                self.x[k] = self.x[k] - self.x[k, j] * self.x[i]
+            
+            pivotes += 1
+            i += 1
+            j += 1
+        
+        for fila in range(m):
+            if np.allclose(self.x[fila, :n], 0) and not np.isclose(self.x[fila, n], 0):
+                return 3    #NO HAY SOLUCION
+        
+        if pivotes < n:
+            return 2    #INFINITAS SOLUCIONES
+        
+        # GAUSS-JORDAN
+        for i in range(m-1, -1, -1):
+            pivot_col = None
+            for j in range(n):
+                if not np.isclose(self.x[i, j], 0):
+                    pivot_col = j
+                    break
+            
+            if pivot_col is None:
+                continue
+            
+            for k in range(i):
+                factor = self.x[k, pivot_col]
+                self.x[k] = self.x[k] - factor * self.x[i]
+            
+            return 1 # SOLUCION UNICA
--- pruebas.py
+++ pruebas.py
@@ -1,12 +1,33 @@
-import GaussJordan as gg
+import GaussJordan as gauss
+import numpy as np
 
 def formato(x):
     x=x.split(';')
-    x=[list(map(int,i.split())) for i in x]
+    x=np.array([list(map(int,i.split())) for i in x])
     return x
 
+subs = "₀₁₂₃₄₅₆₇₈₉"
+
+def subindice(num):
+    return ''.join(subs[int(d)] for d in str(num))
+
+
+gg=gauss.GaussJordan()
 while True:
     x = input("Matriz: ")
     if x=='s': break
     x = formato(x)
     print(x)
+    gg.nuevo(x)
+    e=gg.resolver()
+    if e==1:
+        sol = gg.x[:,-1]
+        for i in range(len(sol)):
+            print(f"x{subindice(i+1)} = {sol[i]}")
+    if e==2:
+         print("Infinitas soluciones")
+        
+    if e==3:
+         print("Infinitas soluciones")
+    
+    print()
